7e95bdf0d3289105aa042bb2dfa09188a4bc2b9e
Walter Nunes Jr <walternj@hotmail.com>
2021-05-18 14:25:34 +0200

    Add files via upload
    initial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SigInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+
+export function Header () {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news"/>
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a  >Posts</a>
+        </nav>
+        <SigInButton />
+      </div>
+    </header>
+  )
+}
